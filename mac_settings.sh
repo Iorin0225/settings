6ec3dd8adb3b3c3bf8fae40e4df6de294f433455
Iori Osada <iori.osada@gi-no.jp>
2019-02-04 18:33:58 +0900

    feat: [mac_settings.sh] remove unused line.

--- mac_settings.sh
+++ mac_settings.sh
@@ -2,4 +2,5 @@ defaults write -g KeyRepeat -int 1
 defaults write -g InitialKeyRepeat -int 15
 defaults write com.apple.finder _FXShowPosixPathInTitle -boolean true
 
-sudo spctl --master-enable
+# You use this when you want to change Security settings
+# sudo spctl --master-enable
